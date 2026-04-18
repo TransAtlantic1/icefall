ad447d81a5cf15ddd48bcf34521e60fd9d7efdcb
TransAtlantic1 <maine.004@sjtu.edu.cn>
2026-04-18 15:32:11 +0000

    Remove obsolete Emilia recipe

--- egs/emilia/ASR/local/compute_emilia_features.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-from compute_fbank_emilia import main
-
-
-if __name__ == "__main__":
-    main()
--- egs/emilia/ASR/local/offline_resample_recordings.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-from resample_recordings_to_flac import main
-
-
-if __name__ == "__main__":
-    main()
